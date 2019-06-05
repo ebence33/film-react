6b42e759dd5131ed43634a6ad2dd6a3eaed9d013
Bence <elo.bence33@gmail.com>
2019-06-05 16:00:40 +0200

    05/06/2019

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from '.composants/Header';
+import Item from '.composants/Item';
+import ListItem from '.composants/ListItem';
+import Popup from '.composants/Popup';
+import Videocurrent from '.composants/Videocurrent';
+import Videodetails from '.composants/Videodetails';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
